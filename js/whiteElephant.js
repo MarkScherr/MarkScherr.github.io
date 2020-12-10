fb983358bbad6d3efdd109bec300ed960f9f0ad1
Mark Scherr <mrscherr@gmail.com>
2020-12-10 13:26:29 -0600

    updated dice

--- js/whiteElephant.js
+++ js/whiteElephant.js
@@ -34,8 +34,8 @@ function setNameEnterButton() {
 
 function newDice() {
 	$("#weDiv").append('<div id="diceDiv" class="col-md-12"></div>');
-	$("#diceDiv").append('<img src="img/d6/g1.jpg" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
-	$("#diceDiv").append('<img src="img/d6/r1.jpg" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
+	$("#diceDiv").append('<img src="img/d6/1.png" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
+	$("#diceDiv").append('<img src="img/d6/1.png" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
 	$("#weDiv").append('<div id="rollContainer"><div id="rollDiceButton" class="col-md-12"><button id="diceButton" class="btn btn-primary" type="submit">ROLL</button></div></div>');
 	$("#weDiv").append('<div id="resultDiv"></div>');
 	setDiceRollButton();
@@ -44,8 +44,8 @@ function newDice() {
 function refreshDice(diceToPlace) {
 	emptyDivs();
 	$("#resultDiv").empty();
-	$("#diceDiv").append('<img src="img/d6/g' + diceToPlace + '.jpg" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
-	$("#diceDiv").append('<img src="img/d6/r' + diceToPlace + '.jpg" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
+	$("#diceDiv").append('<img src="img/d6/' + diceToPlace + '.png" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
+	$("#diceDiv").append('<img src="img/d6/' + diceToPlace + '.png" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
 	setTimerOfDice();
 	$("#rollDiceButton").show();
 }
@@ -62,12 +62,12 @@ function emptyDivs() {
 }
 function rollDice() {
 	emptyDivs();	
-	var greenDie = getRandom(1,6);
-	var redDie = getRandom(1,6);
-	$("#diceDiv").append('<img src="img/d6/g' + greenDie + '.jpg" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
-	$("#diceDiv").append('<img src="img/d6/r' + redDie + '.jpg" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
-	if (greenDie === redDie) {
-		getGiftsFromServer(greenDie);
+	var die1 = getRandom(1,6);
+	var die2 = getRandom(1,6);
+	$("#diceDiv").append('<img src="img/d6/' + die1 + '.png" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
+	$("#diceDiv").append('<img src="img/d6/' + die2 + '.png" style="flex-grow:2;width: 50%;max-width:50%;height:150px;">');
+	if (die1 === die2) {
+		getGiftsFromServer(die1);
 	} else {
 		setTimerOfDice();
 	}
